ff3949c28f99b5d0dc86848ea9e1a247bd8afc21
Tanvir-Jaber <86012209+Tanvir-Jaber@users.noreply.github.com>
2021-06-16 23:46:00 +0600

    Delete views/member directory

--- views/member/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once "stripe-php-master/init.php";
-$Publishablekey = "pk_test_51J0XmZLTphVospsTEPQSPeSeH5DtvRBLNmz3vcI69G9126zHkNhO1AV9E3MWu13oDrPEKUNYERSbvBY895SNQK8P00OlvFyGGx";
-$Secretkey = "sk_test_51J0XmZLTphVospsTHwfkmuFZGSaKtbkmHbvKQqb55sndAzgh5SZ2VmfDRYShMuMpmqzZ3UcXmiHHeMwuVsDMrUga002st8pBqZ";
-\Stripe\Stripe::setApiKey($Secretkey);
--- views/member/view_request.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: hp
- * Date: 13-Jun-21
- * Time: 4:35 AM
- */
